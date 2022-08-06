f59a38ad5ff9c95ead0052254b3ae6850dca72cf
Ian <ian@devpipeline.com>
2022-08-06 14:16:52 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// FE Capstone:
+// Requirements-
+// Render a dashboard w/ cards for widgets.
+// MUST HAVE WIDGETS:
+//  - Weather widget that fetch's from an api
+//  - Swapi Query Widget -should be able to search swapi through your app and display information retrieved
+//  - At least 3 other tools of your choosing
+// About Page with a description of the Capstone and what you took away from this course.
+// Login through custom api https://devpipeline-mock-api.herokuapp.com. A fallback could be  https://httpbin.org/post
+// Logout
+
+// Pages -
+// Render all Widgets on Dashboard (as cards, not the actual widgets themselves).
+// Show Route to individual widget with an explanation of what the widget is.
+// About Page
+// Login Page
+
+// Must use BrowserRouter v5!!!!
+// NavBar
+// Must use SCSS
+// Must use CRA
+// Must be submitted through github link
+// If you can host it, even better.
+
+// Hints:
+// Ask Questions
+// Help each other
+// Utilize the internet!
+// Don't limit yourselves.
+// HAVE FUN!!!
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from App</h1>
+      <Router>
+        <div>
+          <Route></Route>
+        </div>
+      </Router>
     </div>
   );
 }
